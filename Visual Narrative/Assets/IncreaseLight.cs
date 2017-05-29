80ff16c4253948668165ee7ff4bbcab1ec702db4
Ryan Steele <ryan.steele.j@gmail.com>
2017-05-29 15:07:17 -0600

    Start of project

---
+++ Visual Narrative/Assets/IncreaseLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseLight : MonoBehaviour {
+
+    private Light pointLight;
+	// Use this for initialization
+	void Start () {
+        pointLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "player")
+        {
+            Debug.Log("Light Triggered");
+            if (pointLight.intensity < 1)
+            {
+                pointLight.intensity += (Time.deltaTime * 0.5f);
+            }
+        }
+    }
+}
---
+++ Visual Narrative/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D player;
+    private Vector2 characterVector;
+    public float moveSpeed = 25;
+    // Use this for initialization
+    void Start()
+    {
+        player = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale != 0)
+        {
+            characterVector.y = Input.GetAxis("Vertical");
+            characterVector.x = Input.GetAxis("Horizontal");
+            if (player.velocity.magnitude > 0)
+            {
+                player.velocity = Vector2.zero;
+            }
+            if (characterVector.x == 0 && characterVector.y == 0)
+            {
+                player.velocity = Vector2.zero;
+            }
+            player.transform.Translate(characterVector.x * moveSpeed * Time.deltaTime, characterVector.y * moveSpeed * Time.deltaTime, 0);
+        }
+    }
+}
